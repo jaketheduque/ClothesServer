b238cc1083aca79519336649826af282729350e0
BuildTools <jazzyandsaxy6@gmail.com>
2023-03-27 18:06:38 -0700

    Changed path of outfit page back to /outfit

--- src/main/java/me/jaketheduque/controllers/WebController.java
+++ src/main/java/me/jaketheduque/controllers/WebController.java
@@ -38,7 +38,7 @@ public class WebController {
         return "view";
     }
 
-    @RequestMapping("/")
+    @RequestMapping("/outfit")
     public String outfit(Model model) {
         model.addAttribute("outfit_types", outfitTypeRepository.getAllOutfitTypes());
         return "outfit";
